c3c88258fc7dc6a2e170fd26abde9dc56414dc0d
josephine324 <j.mutesi@alustudent.com>
2024-07-08 09:39:57 +0530

    initial commit

---
+++ job_search/src/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAZ7IGSeV9FncKf3k1jq7kPPwvvpDWkzig",
+  authDomain: "online-job-portal-9afa0.firebaseapp.com",
+  projectId: "online-job-portal-9afa0",
+  storageBucket: "online-job-portal-9afa0.appspot.com",
+  messagingSenderId: "664634547931",
+  appId: "1:664634547931:web:5a63a2a78fe0cc4c7c36fe"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export {db};
